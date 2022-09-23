0509afcff433f4f2cae2f0ce9709009430b36514
sawysockii <simon.vysotski@gmail.com>
2022-09-23 21:34:46 +0300

    again

--- hh_parser.py
+++ hh_parser.py
@@ -2,25 +2,44 @@ import requests as req
 from bs4 import BeautifulSoup
 
 
-url = 'https://spb.hh.ru/search/vacancy?area=113&text=python+разработчик'
-sess = req.Session()
-sess.headers.update({
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'ru',
-    'cache-control': 'no-cache',
-    'pragma': 'no-cache',
-    'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Linux"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-})
-data=sess.get(url)
+#url = 'https://spb.hh.ru/search/vacancy?area=113&text=python+разработчик'
+url = 'https://www.avito.ru/all/rabota?q=python'
+headers_list = [{ 
+	'authority': 'httpbin.org', 
+	'cache-control': 'max-age=0', 
+	'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"', 
+	'sec-ch-ua-mobile': '?0', 
+	'upgrade-insecure-requests': '1', 
+	'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36', 
+	'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 
+	'sec-fetch-site': 'none', 
+	'sec-fetch-mode': 'navigate', 
+	'sec-fetch-user': '?1', 
+	'sec-fetch-dest': 'document', 
+	'accept-language': 'en-US,en;q=0.9', 
+} # , {...} 
+] 
+headers = random.choice(headers_list) 
+response = req.get(url, headers=headers) 
 
-soup = BeautifulSoup(data.text,"lxml")
+# sess = req.Session()
+# sess.headers.update({
+#     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+#     'accept-encoding': 'gzip, deflate, br',
+#     'accept-language': 'ru',
+#     'cache-control': 'no-cache',
+#     'pragma': 'no-cache',
+#     'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+#     'sec-ch-ua-mobile': '?0',
+#     'sec-ch-ua-platform': '"Linux"',
+#     'sec-fetch-dest': 'document',
+#     'sec-fetch-mode': 'navigate',
+#     'sec-fetch-site': 'none',
+#     'sec-fetch-user': '?1',
+#     'upgrade-insecure-requests': '1',
+#     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+# })
+# data = sess.get(url)
+
+soup = BeautifulSoup(response.text,"lxml")
 print(soup.text)
