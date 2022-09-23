e60efcc7867bd800b695facde591a90a3b74ccfb
sawysockii <simon.vysotski@gmail.com>
2022-09-23 21:05:01 +0300

    changes

--- hh_parser.py
+++ hh_parser.py
@@ -1,8 +1,26 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 23 11:33:46 2022
+import requests as req
+from bs4 import BeautifulSoup
 
-@author: sa.vysotskiy
-"""
 
+url = 'https://spb.hh.ru/search/vacancy?area=113&text=python+разработчик'
+sess = req.Session()
+sess.headers.update({
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'ru',
+    'cache-control': 'no-cache',
+    'pragma': 'no-cache',
+    'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+})
+data=sess.get(url)
+
+soup = BeautifulSoup(data.text,"lxml")
+print(soup.text)
