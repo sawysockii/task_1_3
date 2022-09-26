19e2d02fb641632b5879ac9a2bbf40dec7006e49
sawysockii <simon.vysotski@gmail.com>
2022-09-26 16:09:44 +0300

    first version palindrom

--- palindrom.py
+++ palindrom.py
@@ -1,33 +1,18 @@
-def only_brackets_in_string(input_string):
-    only_brackets = 1
-    if len(str(input_string)) > 0:
-        for char_i in str(input_string):
-            if char_i in brackets_string:
-                only_brackets *= 1
-            else:
-                only_brackets *= 0
-        if only_brackets == 1:
-            return(True)
-        else:
-            return(False)
-    else:
-        return(False)
-
-
+def palindrom(input_string):
+    return(str(input_string)[::-1])
+    
 run = True
 while run:
-    our_string = input('Введите последовательность скобок: ')
+    our_string = input('Введите cтроку: ')
+    our_string = our_string.replace(' ','').replace(',','').lower()
     if ((our_string == 'exit') or (our_string == 'quit')):
         print('Счастливо!')
         break
     else:
-        if only_brackets_in_string(our_string):
-            if paired_brackets_in_string(our_string):
-                if check_brackets(our_string):
-                    print('Все ok!')
-                else:
-                    print('Неверное расположение скобок')
+        if len(our_string) > 0:
+            if our_string == palindrom(our_string):
+                print('Это палиндром')
             else:
-                print('Ваши скобки скучают без пары')
+                print('Это не палиндром')
         else:
-            print('Должны быть скобки! Попробуйте еще раз.')
+            print('Строка не должна быть пустой')
